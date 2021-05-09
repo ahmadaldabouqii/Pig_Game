ab31cb5443488fc1a7d3cf086ec7c70a6bb3e112
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-05-10 01:16:55 +0300

    finish building game

--- js/app.js
+++ js/app.js
@@ -45,7 +45,6 @@ btnRoll.addEventListener('click', function () {
   if (playing) {
     const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // Display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `./img/dice-${dice}.png`;
 
